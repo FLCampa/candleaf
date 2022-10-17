7bc8216e01f35438ff839d62c8f2943cd413a2fc
FLCampa <flcampanelli@gmail.com>
2022-10-17 16:02:10 -0300

    feat: implements navbar and head content

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,29 @@
+// External Libraries
+import React from "react";
+
+// Stylization
+import { Container, Card } from "./styles";
+
+const Heading: React.FC = () => {
+  return (
+    <Container>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/bg-image.png" alt="background image" />
+
+      <Card>
+        <h2>The nature candle</h2>
+
+        <span>
+          All handmade with natural soy wax, Candleaf is a companion for all
+          your pleasure moments
+        </span>
+
+        <button onClick={() => console.log("scrollToRef")}>
+          Discovery our collection
+        </button>
+      </Card>
+    </Container>
+  );
+};
+
+export default Heading;
---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,36 @@
+// External Libraries
+import Image from "next/image";
+import React from "react";
+
+// Components
+import NavItem from "../NavItem";
+
+// Assets
+import { Profile, Cart } from "../../styles/icons";
+
+// Stylization
+import { Container, Content } from "./styles";
+
+const Nav: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <Image src="/logo.svg" alt="Logo" width={126} height={34} />
+
+        <ul>
+          {/* <MenuItem url="/notifications" text="Notifications" icon={BellIcon} /> */}
+          <NavItem url="/discovery" text="Discovery" />
+          <NavItem url="/about" text="About" />
+          <NavItem url="/contact" text="Contact us" />
+        </ul>
+
+        <ul>
+          <NavItem url="/profile" icon={Profile} />
+          <NavItem url="/cart" icon={Cart} />
+        </ul>
+      </Content>
+    </Container>
+  );
+};
+
+export default Nav;
